c27733ded524ac060b690ab84d321fac8cc3a5c9
setkanet <128897001+setkanet@users.noreply.github.com>
2024-02-29 23:11:57 +0300

    Мелкие исправления

--- templates/layout.php
+++ templates/layout.php
@@ -55,7 +55,7 @@
 				<!--заполните этот список из массива категорий-->
 				<?php foreach ($categories as $category) : ?>
 					<li class="nav__item">
-						<a href="pages/all-lots.html"><?= $category; ?></a>
+						<a href="pages/all-lots.html"><?= $category["name_category"]; ?></a>
 					</li>
 				<?php endforeach; ?>
 			</ul>
